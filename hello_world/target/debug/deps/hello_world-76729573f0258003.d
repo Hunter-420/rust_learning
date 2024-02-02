9ec5b41c9711aed8a641fb0cfeb6eb88bc9f65e3
Niachal Khanal <khanalnischal2075@gmail.com>
2024-02-02 17:22:50 +0545

    chapter 1 hello world

---
+++ hello_world/target/debug/deps/hello_world-76729573f0258003.d
@@ -0,0 +1,5 @@
+/home/hunter420/Desktop/rust_learning/hello_world/target/debug/deps/hello_world-76729573f0258003: src/main.rs
+
+/home/hunter420/Desktop/rust_learning/hello_world/target/debug/deps/hello_world-76729573f0258003.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/hunter420/Desktop/rust_learning/hello_world/target/debug/hello_world: /home/hunter420/Desktop/rust_learning/hello_world/src/main.rs
