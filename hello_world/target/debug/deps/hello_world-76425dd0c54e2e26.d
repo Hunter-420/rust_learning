72986472430d3b79c10fbe121a464fb6d35e2a30
Niachal Khanal <khanalnischal2075@gmail.com>
2024-02-03 09:51:23 +0545

    chapter 1 hello_world

---
+++ hello_world/target/debug/deps/hello_world-76425dd0c54e2e26.d
@@ -0,0 +1,9 @@
+/home/hunter420/Desktop/rust_learning/hello_world/target/debug/deps/libhello_world-76425dd0c54e2e26.rmeta: src/main.rs Cargo.toml
+
+/home/hunter420/Desktop/rust_learning/hello_world/target/debug/deps/hello_world-76425dd0c54e2e26.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
